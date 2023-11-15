3ebb91d10fc6ade3a4e9384124fb8a0110c69db4
NkustMap <138897165+NkustMap@users.noreply.github.com>
2023-11-15 19:16:08 +0800

    Update floor.js

--- js/floor.js
+++ js/floor.js
@@ -1,22 +1,3 @@
-<!-- 漢堡選單程式 -->
-	const hamburger = document.querySelector(".hamburger");
-	const navMenu = document.querySelector(".nav-menu");
-
-	hamburger.addEventListener("click", mobileMenu);
-
-	function mobileMenu() {
-		hamburger.classList.toggle("active");
-		navMenu.classList.toggle("active");
-	}
-	const navLink = document.querySelectorAll(".nav-link");
-
-	navLink.forEach(n => n.addEventListener("click", closeMenu));
-
-	function closeMenu() {
-		hamburger.classList.remove("active");
-		navMenu.classList.remove("active");
-	}
-
 function showPopup(imageSrc, text) {
             const modal = document.getElementById('myModal');
             const modalImage = document.getElementById('modalImage');
