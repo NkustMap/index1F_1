7ba4d852cab8b8ca4527e055a518b8aa176822e3
NkustMap <138897165+NkustMap@users.noreply.github.com>
2023-11-15 19:15:14 +0800

    Update other.js

--- js/other.js
+++ js/other.js
@@ -1,23 +1,4 @@
-<!-- 漢堡選單程式 -->
-	const hamburger = document.querySelector(".hamburger");
-	const navMenu = document.querySelector(".nav-menu");
-
-	hamburger.addEventListener("click", mobileMenu);
-
-	function mobileMenu() {
-		hamburger.classList.toggle("active");
-		navMenu.classList.toggle("active");
-	}
-	const navLink = document.querySelectorAll(".nav-link");
-
-	navLink.forEach(n => n.addEventListener("click", closeMenu));
-
-	function closeMenu() {
-		hamburger.classList.remove("active");
-		navMenu.classList.remove("active");
-	}
-	
-	<!-- 客服 -->
+<!-- 客服 -->
 
 	var qaList = [
 	{Q:"你好|哈囉|嗨|哈羅|嗨嗨|HI|Hi|HELLO|Hello", A:"您好!請問需要什麼幫助呢?"},
